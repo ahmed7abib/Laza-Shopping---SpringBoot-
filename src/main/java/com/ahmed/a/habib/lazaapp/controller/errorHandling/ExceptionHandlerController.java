e9eac0b6e6f2fca429f20f0021e3844fa9b3cac1
ahmed.habib <ahmed.habib@sewedy.com.eg>
2024-11-12 15:30:14 +0200

    Created one-to-many relation between user table and otp table

--- src/main/java/com/ahmed/a/habib/lazaapp/controller/errorHandling/ExceptionHandlerController.java
+++ src/main/java/com/ahmed/a/habib/lazaapp/controller/errorHandling/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.ahmed.a.habib.lazaapp.controller.errorHandling;
 
+import com.ahmed.a.habib.lazaapp.model.response.ExceptionResponse;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,24 +8,24 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * RestControllerAdvice for applying to all controllers
  */
+
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<HashMap<String, List<String>>> handleBindException(BindException bindException) {
+    public ResponseEntity<ExceptionResponse> handleBindException(BindException bindException) {
         List<String> errorsList = bindException.getAllErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
 
-        HashMap<String, List<String>> errorMap = new HashMap<>();
-        errorMap.put("errors", errorsList);
-        return new ResponseEntity<>(errorMap, HttpStatus.OK);
+        String error = String.join(", ", errorsList);
+        ExceptionResponse exceptionResponse = new ExceptionResponse(error);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
     }
 }
---
+++ src/main/java/com/ahmed/a/habib/lazaapp/model/response/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package com.ahmed.a.habib.lazaapp.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String exceptionMsg;
+}
